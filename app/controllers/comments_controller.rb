8fd7cee209c41357bacc802c3c9d6856217663bb
moriyama07ichino <chikano.kato.07@gmail.com>
2024-07-08 21:46:39 +0900

    修正_コメントフォームを空のまま投稿しようとすると、投稿できずにそのページに留まるように修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
-  def create
+   def create
     @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])
+  
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
     end
